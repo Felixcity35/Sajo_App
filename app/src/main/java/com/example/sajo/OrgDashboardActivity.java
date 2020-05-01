a65e8c1ecdd41a2f55c6f2ee9803e79133ac3225
Precious John <preciousinyang10@gmail.com>
2020-05-01 01:30:48 +0100

    database added

---
+++ app/src/main/java/com/example/sajo/OrgDashboardActivity.java
@@ -0,0 +1,18 @@
+package com.example.sajo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class OrgDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_org_dashboard);
+
+
+
+    }
+}
